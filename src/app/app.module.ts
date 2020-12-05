2ad7eb16c7daa7fe87bdc6fc4057742659424625
Muhammad Usman <mu08697@gmail.com>
2020-12-05 17:15:56 +0500

    angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,24 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './template/home/home.component';
+import { Home1Component } from './template/home1/home1.component';
+import { Home2Component } from './template/home2/home2.component';
+import { CarComponent } from './component/car/car.component';
+import { BusComponent } from './component/bus/bus.component';
+import { TruckComponent } from './component/truck/truck.component';
+import { BikeComponent } from './component/bike/bike.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    Home1Component,
+    Home2Component,
+    CarComponent,
+    BusComponent,
+    TruckComponent,
+    BikeComponent
   ],
   imports: [
     BrowserModule,
